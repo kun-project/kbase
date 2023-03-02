eb9ad98e0d322b7f001e30df09c1d2a23a53358e
zhurong <211103376@qq.com>
2023-03-02 12:08:37 +0800

    使用 xmake

---
+++ xmake.lua
@@ -0,0 +1,18 @@
+add_requires("gtest")
+add_rules("mode.profile", "mode.releasedbg", "mode.release")
+
+set_languages("c17", "cxx17")
+
+if is_plat("windows") then 
+    add_cxxflags("/Zc:__cplusplus")
+end
+
+target("kbase")
+    set_kind("headeronly")
+    add_includedirs("include/", { public = true })
+
+target("kbase_test")
+    set_kind("binary")
+    add_files("tests/*.cpp")
+    add_deps("kbase") 
+    add_packages("gtest")
